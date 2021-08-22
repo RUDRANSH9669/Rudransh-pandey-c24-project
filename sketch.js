0264de4e5122d62d6d66a2a404afda2360e9aad3
RUDRANSH9669 <86914487+RUDRANSH9669@users.noreply.github.com>
2021-08-22 16:58:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var tower;
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  tower = new Tower(150, 350, 160, 310);
+  cannon=new Cannon(180,110,100,50,-PI/4)
+
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+    tower.display();
+    cannon.dislay
+
+ 
+}
+
